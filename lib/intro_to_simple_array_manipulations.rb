d9e5507be358b695465be4e5e67a0ea9db2303c0
Mike Archer <mlarcher531@gmail.com>
2020-07-29 14:50:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,6 @@
 def using_concat
-  array.concat
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat([more_favs])
+  
 end
