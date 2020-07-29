22d2a52eafed8b83984740cbb1fbe8e7ad9f6f3c
Mike Archer <mlarcher531@gmail.com>
2020-07-29 15:17:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+  p my_favorite_things
+end
+
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
+  p list_of_programming_languages
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq!
+  p haircuts
+end
+
+def using_flatten(instruments)
+  instruments.flatten!
+  p instruments
+end
+
+def using_delete(instructors, steven)
+  instructors.delete("Steven")
+  p instructors
+end
+
+def using_delete_at(array,interger)
+  array.delete_at(2)
+end
